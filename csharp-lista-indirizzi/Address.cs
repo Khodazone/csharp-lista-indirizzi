73c35eb5749525c128240f14aac32ad449c8c780
Khodazone <khodazone@gmail.com>
2023-04-26 17:31:54 +0200

    address

---
+++ csharp-lista-indirizzi/Address.cs
@@ -0,0 +1,34 @@
+﻿public class Address
+    {
+        //Attributes
+        public string Name { get; }
+        public string Surname { get; }
+        public string Street { get; }
+        public string City { get; }
+        public string Province { get; }
+        public int Zip { get; }
+
+        // Constructor
+        public Address(string name, string surname, string street, string city, string province, int zip)
+        {
+            this.Name = name;
+            this.Surname = surname;
+            this.Street = street;
+            this.City = city;
+            this.Province = province;
+            this.Zip = zip;
+        }
+        public override string ToString()
+        {
+            string riga = "";
+            riga += "Name: " + this.Name + "\n";
+            riga += "Surname: " + this.Surname + "\n";
+            riga += "Street: " + this.Street + "\n";
+            riga += "City: " + this.City + "\n";
+            riga += "Province: " + this.Province + "\n";
+            riga += "ZIP: " + this.Zip + "\n";
+            riga += "---------------------------------------------------";
+
+            return riga;
+        }
+    }
--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,23 +1,34 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 // Open the file to read from.
-StreamReader file = File.OpenText("C:\\Users\\khoda\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\Cartella Indirizzi\\addresses.csv");
-while (!file.EndOfStream)
+StreamReader addresses = File.OpenText("C:\\Users\\khoda\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\Cartella Indirizzi\\addresses.csv");
+
+addresses.ReadLine();
+while (!addresses.EndOfStream)
 {
-    string riga = file.ReadLine();
-    Console.WriteLine(riga);
+    string riga = addresses.ReadLine();
+    string[] split = riga.Split(",");
+
+    try
+    {
+        string Name = split[0];
+        string Surname = split[1];
+        string Street = split[2];
+        string City = split[3];
+        string Province = split[4];
+        int Zip = int.Parse(split[5]);
+
+        // Print single address
+        Address address = new Address(Name, Surname, Street, City, Province, Zip);
+
+        Console.WriteLine(address);
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine("Qualcosa non ha funzionato...");
+        Console.WriteLine("L'errore è: " + e.Message);
+    }
 }
-file.Close();
-
-
-
-
-
-
-
-
-
-
-
+addresses.Close();
 
 
