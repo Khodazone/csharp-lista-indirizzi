f822a7394c1bd8b020c565628dbdb2d0321818c5
Khodazone <khodazone@gmail.com>
2023-04-26 15:32:04 +0200

    start

--- csharp-lista-indirizzi/Program.cs
+++ csharp-lista-indirizzi/Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Open the file to read from.
+StreamReader file = File.OpenText("C:\\Users\\khoda\\source\\repos\\csharp-lista-indirizzi\\csharp-lista-indirizzi\\Cartella Indirizzi\\addresses.csv");
+while (!file.EndOfStream)
+{
+    string riga = file.ReadLine();
+    Console.WriteLine(riga);
+}
+file.Close();
+
+
+
+
+
+
+
+
+
+
+
+
+
